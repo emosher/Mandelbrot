0f8b2a868d7c30f364abf793ca55f08305d0ee7b
emosher <eric@home.com>
2012-07-06 22:45:54 -0400

    Updating color schemes with comments to tell what they are.

--- ColorScheme.java
+++ ColorScheme.java
@@ -26,13 +26,20 @@ public class ColorScheme {
     public int getColor(int i) {
         // A color scheme
         int a = (int) (255 * ((double) i) / (maxIterations / 4));
-        return //( (2*a<<16) );
-        // Other options of varying qualities...
-                //( (255 * (i/15)) << 16 | (255 * (i/15)) );
-                //((255 * (i/20)) << 16 | 0 | 0 );
-                //((255 * (i/10)) << 16 | (255 * (i/10)) << 8 | (255 * (i/10)) );
-                (65536 + i*256 + i/2+128);
-                //( (0) | (2*a<<16) | (a<<8) | ((a*2)<<0) );
+        return 
+                // Red & black with fade, a classic!
+                // ( (2*a<<16) );
+                // Other options of varying qualities...
+                // Hot pink bar & black
+                // ( (255 * (i/15)) << 16 | (255 * (i/15)) );
+                // Red bars & black
+                // ((255 * (i/20)) << 16 | 0 | 0 );
+                // The cow level! Black & white bars
+                // ((255 * (i/10)) << 16 | (255 * (i/10)) << 8 | (255 * (i/10)) );
+                // Blue, blue-green fade, and black
+                // (65536 + i*256 + i/2+128);
+                // Black & purple/pink fade
+                ( (0) | (2*a<<16) | (a<<8) | ((a*2)<<0) );
     }
 
 }
